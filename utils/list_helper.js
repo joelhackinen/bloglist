53f82f5d88ad5714bb602dc75af7272f8810dac0
Joel <joel.hakkinen@aalto.fi>
2022-09-27 21:13:08 +0300

    4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -6,8 +8,21 @@ const totalLikes = (listOfBlogs) => {
   return listOfBlogs.reduce((sum, next) => sum + next.likes, 0)
 }
 
+const favouriteBlog = (listOfBlogs) => {
+  return listOfBlogs.reduce((prev, current) => (prev.likes > current.likes) ? prev : current)
+}
+
+const mostBlogs = (listOfBlogs) => {
+  const authors = _.countBy(listOfBlogs, 'author')
+  const zipped = _.zip(Object.keys(authors), Object.values(authors))
+  const asList = zipped.reduce((prev, current) => (prev[1] > current[1]) ? prev : current)
+  return {author: asList[0], blogs: asList[1]}
+}
+
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog,
+  mostBlogs
 }
